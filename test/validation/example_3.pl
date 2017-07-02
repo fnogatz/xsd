0be80264a6450f6e964fa316f3067fe0c94717fa
Falco Nogatz <fnogatz@gmail.com>
2017-07-03 00:48:59 +0200

    Add another (working, yeah!) example

---
+++ test/validation/example_3.pl
@@ -0,0 +1,16 @@
+'Example 3':
+{|xml||
+	<?xml version="1.0" encoding="UTF-8"?>
+	<order>
+		<user>
+			<fullname>Bob Jones</fullname>
+			<deliveryAddress>123 This road,
+	            That town,
+	            Bobsville</deliveryAddress>
+		</user>
+		<products>
+			<product id="12345" quantity="1" />
+			<product id="3232" quantity="3" />
+		</products>
+	</order>
+|}.
