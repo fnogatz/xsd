6e79b9986c47fca3e80df1c27166a92fb55f34cf
Falco Nogatz <fnogatz@gmail.com>
2017-07-03 20:36:59 +0200

    v0.1.0

---
+++ pack.pl
@@ -0,0 +1,13 @@
+name(xsd).
+
+version('0.1.0').
+download('https://github.com/jonakalkus/swipl-xsd/archive/v0.1.0.zip').
+
+title('XML Schema validation with Prolog').
+author('Jona Kalkus','jonakalkus@gmail.com').
+author('Falco Nogatz','fnogatz@gmail.com').
+maintainer('Falco Nogatz','fnogatz@gmail.com').
+maintainer('Jona Kalkus','jonakalkus@gmail.com').
+packager('Falco Nogatz','fnogatz@gmail.com').
+
+home('https://github.com/jonakalkus/swipl-xsd').
