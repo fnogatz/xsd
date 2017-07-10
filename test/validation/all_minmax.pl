687b6eceacc9ba8d72b1a74881faaaeece45df68
jonakalkus <jonakalkus@gmail.com>
2017-07-10 19:02:37 +0200

    Some Test Cases
    * Another more complex examle, similar to the previous example.xsd, but
    using named types.

--- test/validation/all_minmax.pl
+++ test/validation/all_minmax.pl
@@ -9,5 +9,62 @@
 
 'Valid all #2':
 {|xml||
+	<all>
+		<e0 />
+		<e2 />
+		<e3 />
+	</all>
+|}.
+
+'Valid empty all':
+{|xml||
 	<all />
 |}.
+
+'Missing elements #1'(fail):
+{|xml||
+	<all>
+		<e0 />
+	</all>
+|}.
+
+'Missing elements #2'(fail):
+{|xml||
+	<all>
+		<e0 />
+		<e3 />
+	</all>
+|}.
+
+
+'Too many elements #1'(fail):
+{|xml||
+	<all>
+		<e2 />
+		<e0 />
+		<e3 />
+		<unknown />
+	</all>
+|}.
+
+'Too many elements #2'(fail):
+{|xml||
+	<all>
+		<e2 />
+		<e0 />
+		<e3 />
+		<e2 />
+	</all>
+|}.
+
+'Too many elements #3'(fail):
+{|xml||
+	<all>
+		<e0 />
+		<e1 />
+		<e2 />
+		<e3 />
+	</all>
+|}.
+
+
