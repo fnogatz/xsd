5eb93aeacc86514bea46c4940dbeda19c5e2f04e
Falco Nogatz <fnogatz@gmail.com>
2020-10-12 03:01:36 +0200

    v0.2.1

--- pack.pl
+++ pack.pl
@@ -1,13 +1,12 @@
 name(xsd).
 
-version('0.2.0').
+version('0.2.1').
 download('https://github.com/jonakalkus/xsd/releases/*.zip').
 
 title('XML Schema validation with Prolog').
 author('Jona Kalkus','jonakalkus@gmail.com').
 author('Falco Nogatz','fnogatz@gmail.com').
 maintainer('Falco Nogatz','fnogatz@gmail.com').
-maintainer('Jona Kalkus','jonakalkus@gmail.com').
 packager('Falco Nogatz','fnogatz@gmail.com').
 
 home('https://github.com/jonakalkus/xsd').
