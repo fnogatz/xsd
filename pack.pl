15ec3f466339e25e75cc4753a21cc547293b9d09
Falco Nogatz <fnogatz@gmail.com>
2020-10-22 00:35:56 +0200

    Reflect owner transfer of GitHub repository in pack.pl

--- pack.pl
+++ pack.pl
@@ -1,15 +1,14 @@
 name(xsd).
 
 version('0.2.1').
-download('https://github.com/jonakalkus/xsd/releases/*.zip').
+download('https://github.com/fnogatz/xsd/releases/*.zip').
 
 title('XML Schema validation with Prolog').
-author('Jona Kalkus','jonakalkus@gmail.com').
 author('Falco Nogatz','fnogatz@gmail.com').
 maintainer('Falco Nogatz','fnogatz@gmail.com').
 packager('Falco Nogatz','fnogatz@gmail.com').
 
-home('https://github.com/jonakalkus/xsd').
+home('https://github.com/fnogatz/xsd').
 
 requires(regex).
 requires(tap).
